8d585c2dc50cbadd2fac43ae881141f6c1929ba6
jisoooit <ydsn336@naver.com>
2022-09-20 16:26:18 +0900

    feat: Update GitTest.java
    add one comment

--- Java/src/me/java/day1/GitTest.java
+++ Java/src/me/java/day1/GitTest.java
@@ -3,5 +3,6 @@ package me.java.day1;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("깃 실험");
+        System.out.println("인텔리제이 커밋 파트에는 add없이 변경파일 선택을 통해서 커밋하는 것인가");
     }
 }
