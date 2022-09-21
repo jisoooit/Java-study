37bd27c2d5395cc9820d9ecb7e79ab63ca87b672
jisoooit <ydsn336@naver.com>
2022-09-21 18:34:47 +0900

    feat: Do practice3 on Day3
    calculator

---
+++ Java/src/me/java/day3/Practice3.java
@@ -0,0 +1,29 @@
+package me.java.day3;
+
+import java.util.Scanner;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        String op = sc.next();
+
+        if (op.equals("+")){
+            System.out.println(num1+num2);
+        }else if( op.equals("-")) {
+            System.out.println(num1 - num2);
+        }else if(op.equals("*")) {
+            System.out.println(num1 * num2);
+        }else if(op.equals("/")) {
+            System.out.println(num1/num2);
+         }else if (op.equals("%")) {
+            System.out.println(num1%num2);
+        }else{
+            System.out.println("연산자가 유효하지 않습니다.");
+        }
+
+
+    }
+}
