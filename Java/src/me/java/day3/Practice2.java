46ac808482f504b39a5d140024f7616029af1f6b
jisoooit <ydsn336@naver.com>
2022-09-21 20:46:34 +0900

    refactor: reduce code
    Make the code more clean

--- Java/src/me/java/day3/Practice2.java
+++ Java/src/me/java/day3/Practice2.java
@@ -8,32 +8,17 @@ public class Practice2 {
 
         Scanner sc = new Scanner(System.in);
 
-        String alpha  = sc.next();
-
-        char ch = alpha.charAt(0);
+        char ch = sc.next().charAt(0);
         if ((ch>='a' && ch <='z') || (ch >='A' && ch <='Z')){
             if ((ch>='a' && ch <='z')){
-                alpha=""+(char)(ch-32);
-                System.out.println(alpha);
+                System.out.println((char)(ch-32));
             }
         }
         else{
             System.out.println("영문자가 유효하지 않습니다.");
-            }
-        /*for (int i = 0 ; i< alpha.length() ; i++){
-            char ch = alpha.charAt(i);
-            if ((ch>='a' && ch <='z') || (ch >='A' && ch <='Z')){
-                System.out.println("영문자가 유효하지 않습니다.");
-                break;
-            }
-            else{
-                if (ch>='a' && ch <='z'){
-
-                    alpha.replace(ch,(char)ch -32);
-                }
-
-                }
-            }*/
         }
+
+
+    }
 
 }
