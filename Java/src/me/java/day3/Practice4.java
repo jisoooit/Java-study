7ea83cb9222befbbccdb989e797fa2150f13b178
jisoooit <ydsn336@naver.com>
2022-09-21 18:35:27 +0900

    feat: Do practice4 on Day3
    plus minus

---
+++ Java/src/me/java/day3/Practice4.java
@@ -0,0 +1,32 @@
+package me.java.day3;
+
+import java.util.Scanner;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextInt();
+        double y = sc.nextInt();
+
+        if ( x>0 && y>0 ) {
+            System.out.println("1 사분면");
+        }
+        else if (x>0 && y<0){
+            System.out.println("2 사분면");
+        }
+        else if (x<0 && y>0){
+            System.out.println("3 사분면");
+        }
+        else if (x<0 && y<0){
+            System.out.println("4 사분면");
+        }else if (x==0 || y==0) {
+            if (x==0){
+                System.out.println("x축 위에 있음");
+            }
+            if (y==0){
+                System.out.println("y축 위에 있음");
+            }
+        }
+    }
+}
